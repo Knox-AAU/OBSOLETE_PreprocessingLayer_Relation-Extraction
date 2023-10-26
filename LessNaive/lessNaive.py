f01097efcd9b734a89dd53c15e2ee6c8b9198788
Jojokp2000 <johannes.karstoft.pedersen@gmail.com>
2023-10-26 15:30:54 +0200

    Remove colon

--- LessNaive/lessNaive.py
+++ LessNaive/lessNaive.py
@@ -12,6 +12,7 @@ def extract_specific_relations():
             # Check if the line starts with a colon and the next lines contain the specified pattern
             if line.startswith(":") and i+1 < len(lines) and "a rdf:Property, owl:ObjectProperty ;" in lines[i+1]:
                 relation = line.split()[0]  # Extracting the relation name
+                relation = relation[1:]
                 relations.add(relation)
             i += 1
             
